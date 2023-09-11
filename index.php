2eaa37f6823200e17a73e5a23ad6372ef4403d65
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-09-10 21:49:35 -0700

    Update index.php
    CONDITIONS AND BOOLEAN EXPRESSIONS

--- index.php
+++ index.php
@@ -5,14 +5,28 @@
     <meta charset="UTF-8">
 
     <title>Demo</title>
+    <style>
+      Body{
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+      }
+      </style>
       </head>
   <body>
-    <h1>
-        <?php
-        echo "Hello World";
+     <?php
+       $name = "Dark Matter";
+       $read = true;
+       if($read){
+        $message = "You have read $name";
+       }else {
+        $message = "You have NOT read $name";
+       }
         ?>
-
-
+    <h1>
+       <?php echo $message; ?>
     </h1>
       </body>
 </html>
