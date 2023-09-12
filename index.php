7bcf6396a9af7173f6f1065d18c3c6b09566899e
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-09-11 18:54:15 -0700

    Update index.php
    just call the index.view.php

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-
-    <title>Demo</title>
-     </head>
-  <body>
-   <ul>
-  <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
-       <li>
-    <li>
-      <a href="<?= $book['purchaseURL']?>">
-    <?=$book['name']; ?>(<?=$book['releaseYear']?>) -By <?=$book['author']?>
-      </a>
-  </li>
-  <?php endforeach;?>
-   
-</ul>
+<?php
+       
      
-      </body>
-</html>
-//A new file for php  
+
+
+
+  require "index.view.php";
+
